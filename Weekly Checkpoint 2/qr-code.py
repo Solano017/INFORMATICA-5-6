085faa30a195531ce0e8f221df8f4ef2247abf46
Solano017 <solanoabdiel04@gmail.com>
2026-08-20 20:08:03 +0000

    qrcode

---
+++ Weekly Checkpoint 2/qr-code.py
@@ -0,0 +1,13 @@
+import qrcode
+def main():
+    song = "https://youtu.be/aDCcLQto5BM?si=Hn3GwkBZhX_a1Rgb"
+    qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+    qr.add_data("song")
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("my-qrcode.png")
+
+
+if __name__ == "__main__":
+    main()
