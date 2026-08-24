689bf50a8b19e2afecde75d1c4c8c18d146cc07d
Solano017 <solanoabdiel04@gmail.com>
2026-08-24 19:23:24 +0000

    password

---
+++ Weekly Checkpoint 2/password.py
@@ -0,0 +1,9 @@
+def main():
+    password = "pepinillo 4life"
+    attempt = (input("What is your password? "))
+    if(attempt == password):
+        print("Correct Password")
+    print("See you later")
+
+if __name__ == "__main__":
+    main()
